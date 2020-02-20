8aa5cbe4e8e34928558e0e1df9f468aa97e663a9
Alena Rebkavets <archirebkavets@gmail.com>
2020-02-20 10:22:42 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+ n = Math.abs(n);
+    let a = String(n);
+ 
+    let arr = [];
+ arr = a.split("");
+  arr.reverse();
+  a = arr.join("");
+    a = Number(a);
+  return a;
 }
